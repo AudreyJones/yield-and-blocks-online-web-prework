550b583ab374afcc948d7cfe9e7577c38dd7a58b
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-06 17:19:26 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,27 @@
-def hello_t
+def hello_t(array)
 
+# if....else conditionals allows for an option of No Block Given and a "graceful failure" 
+  if block_given?
+# Counter variable 
+    i = 0
+  
+    while i < array.length
+# Yield each successive value stored in the array to the passed-in block.
+# "Here, we use the bracket ([]) method to grab the value of each successive index element as we proceed through our while loop, yielding each to a block."
+      yield array[i]
+      i = i + 1
+    end
+# Return the original array!
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
+# Part One: Method Call
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
 
